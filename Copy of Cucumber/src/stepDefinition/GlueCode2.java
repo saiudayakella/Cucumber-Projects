8a0af58b56d6df3fb1ca4f30995e0d193cb0f608
Sai <saiudayakella@gmail.com>
2016-06-17 15:45:02 +1000

    Add another step definition

---
+++ Copy of Cucumber/src/stepDefinition/GlueCode2.java
@@ -0,0 +1,28 @@
+package stepDefinition;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import cucumber.api.java.en.But;
+import cucumber.api.java.en.Then;
+
+
+
+public class GlueCode2 {
+	WebDriver driver;
+
+	/*@Then("^User should login to the web page without any issues$")
+	public void user_should_login_to_the_web_page_without_any_issues() throws Throwable {
+		WebDriverWait wait=new WebDriverWait(driver,5);
+		wait.until(ExpectedConditions.titleContains("Dashboard"));
+		driver.quit();
+	   }*/
+
+
+
+
+
+}
